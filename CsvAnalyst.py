c31a13469ab1027bb40425c0e834b0e403b77595
ppavlukovetc <pavlukovetc@mail.ru>
2022-10-04 01:13:58 +0300

    Создание основы программы

---
+++ CsvAnalyst.py
@@ -0,0 +1,10 @@
+# Программа анализа .csv  фйлов
+import tkinter as tk
+
+#Создание главного окна
+window=tk . Tk ( )
+window. geometry ( "550x550" )
+window. title ( "Программа анализа .csv  фйлов" )
+
+#Запуск цикла mainloop
+window. mainloop ( )
